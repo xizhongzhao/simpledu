0a5a9378afd01fa544c5b78dee1b9d7a44cffe91
hades_zhao <hades_zhao@126.com>
2018-01-13 13:26:48 +0800

    version2

---
+++ simpledu/app.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+from simpledu.config import configs
+from simpledu.models import db,Course
+
+def create_app(config):
+    """ """
+    app = Flask(__name__)
+    app.config.from_object(configs.get(config))
+    db.init_app(app)
+    
+    @app.route('/')
+    def index():
+        courses = Course.query.all()
+        return render_template('index.html',courses=courses)
+    
+    @app.route('/admin')
+    def admin_index():
+        return 'admin'
+
+    return app

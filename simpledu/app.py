4e60d08aa7abd31ce1608b7b0a2bd9b3064849f8
hades_zhao <hades_zhao@126.com>
2018-01-15 22:26:28 +0800

    version16

--- simpledu/app.py
+++ simpledu/app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,Blueprint
 from simpledu.config import configs
 from simpledu.models import db,Course,User
+from flask_migrate import Migrate
 
 def register_blueprints(app):
     from .handlers import front,course,admin,user
@@ -15,6 +16,7 @@ def create_app(config):
     app = Flask(__name__)
     app.config.from_object(configs.get(config))
     db.init_app(app)
+    Migrate(app,db)
     register_blueprints(app)
     
     return app
